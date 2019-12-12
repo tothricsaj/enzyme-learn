6b9f9c129b7e58de972603da149a728548a87362
tothricsaj <tothricsaj@gmail.com>
2019-12-12 13:00:46 +0100

    test cases are run correctly :bus:

--- src/__tests__/Foo.test.js
+++ src/__tests__/Foo.test.js
@@ -5,6 +5,18 @@ import Foo from '../Foo.js';
 
 describe('A suite yheaa', () => {
     it('should render without error', () => {
-        expect(shallow(<Foo />).contains(<div> className="foo">Bar</div>)).toBe(true);
+        expect(shallow(<Foo />).contains(<div className="foo">Bar</div>)).toBe(true);
+    });
+
+    it('should be selectable by class foo', () => {
+        expect(shallow(<Foo />).is('.foo')).toBe(true);
+    });
+
+    it('should be mount in a full DOM', () => {
+        expect(mount(<Foo />).find('.foo').length).toBe(1);
+    });
+
+    it('should render tot static HTML', () => {
+        expect(render(<Foo />).text()).toEqual('Bar');
     });
 });
