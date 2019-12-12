082026e6913f8367eddef1a3ee51211ce36de39a
tothricsaj <tothricsaj@gmail.com>
2019-12-12 12:14:41 +0100

    first test case with enzyme

--- src/__tests__/Foo.test.js
+++ src/__tests__/Foo.test.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
 
+import Foo from '../Foo.js';
+
+describe('A suite yheaa', () => {
+    it('should render without error', () => {
+        expect(shallow(<Foo />).contains(<div> className="foo">Bar</div>)).toBe(true);
+    });
+});
